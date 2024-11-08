203dc78ec5ae72c581815890acb2d57df4de776f
Andrew <andrew.kravchuk@sdktek.com>
2024-11-08 15:00:02 -0700

    Problems solved on Nov 8th

--- temp.py
+++ temp.py
@@ -1,1 +1,27 @@
-print( 1 % 10 ) 
+test_string =  "MCMXCIV" # IV
+            #   MCMXCIV
+
+
+def romanToInt(s: str) -> list:
+    #reversed_s = s[::-1] 
+    values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+    }
+    res = 0
+    result_list = list(map(lambda i:  
+                            -values[ s[i]]
+                            if i < len(s) - 1 and values[ s[i]] < values[s[i+1]] else values[ s[i]]
+                             , range(len(s)) ))
+    
+    return [result_list, sum(result_list)]
+
+
+print(romanToInt(test_string))
+        
+
